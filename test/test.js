5cfa1e915d137598813f0c8e8eaff55eb87b77ae
HEISSLER <h.eissler@de.ibm.com>
2016-11-17 10:28:42 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -9,21 +9,21 @@ describe("Hello World", function() {
   describe("GET /", function() {
 
 
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        //expect(response.statusCode).toBe(200);
-        assert.equal(200, response.statusCode);
-        done();
-      });
-    });
+//    it("returns status code 200", function(done) {
+//      request.get(base_url, function(error, response, body) {
+//        //expect(response.statusCode).toBe(200);
+//        assert.equal(200, response.statusCode);
+//        done();
+//      });
+//    });
 
-    it("returns Hello World", function(done) {
-      request.get(base_url, function(error, response, body) {
-        //expect(body).toBe("Hello World");
-        assert.equal("hello world", body);
-        done();
-      });
-    });
+//    it("returns Hello World", function(done) {
+//      request.get(base_url, function(error, response, body) {
+//        //expect(body).toBe("Hello World");
+//        assert.equal("hello world", body);
+//        done();
+//      });
+//    });
 
     it("renders successfully", function(done) {
     request(app).get('/').expect(200, done);

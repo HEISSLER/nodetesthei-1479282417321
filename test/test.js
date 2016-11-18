76c7f13ea30495ddd2805f18b0c96dc2d1609d03
HEISSLER <h.eissler@de.ibm.com>
2016-11-18 10:21:52 +0100

    added istanbul for code coverage

--- test/test.js
+++ test/test.js
@@ -10,26 +10,26 @@ describe("Hello World", function() {
     it("renders successfully", function(done) {
       request(app).get('/').expect(200, done);
     });
-    
+
     it("hello", function(done) {
     request(app).get('/').expect("hello world", done);
     });
 
-//    it("returns status code 200", function(done) {
-//      request.get(base_url, function(error, response, body) {
-//        //expect(response.statusCode).toBe(200);
-//        assert.equal(200, response.statusCode);
-//        done();
-//      });
-//    });
+    //    it("returns status code 200", function(done) {
+    //      request.get(base_url, function(error, response, body) {
+    //        //expect(response.statusCode).toBe(200);
+    //        assert.equal(200, response.statusCode);
+    //        done();
+    //      });
+    //    });
 
-//    it("returns Hello World", function(done) {
-//      request.get(base_url, function(error, response, body) {
-//        //expect(body).toBe("Hello World");
-//        assert.equal("hello world", body);
-//        done();
-//      });
-//    });
-    
+    //    it("returns Hello World", function(done) {
+    //      request.get(base_url, function(error, response, body) {
+    //        //expect(body).toBe("Hello World");
+    //        assert.equal("hello world", body);
+    //        done();
+    //      });
+    //    });
+        
   });
 });

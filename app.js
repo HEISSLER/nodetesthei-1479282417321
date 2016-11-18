abd453d676638820dee04f4e509bd0b44a5f0a04
HEISSLER <h.eissler@de.ibm.com>
2016-11-18 12:56:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+app.get('/hello', function (req, res) {
+  res.send('hello hello')
+})
+
 app.get(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry

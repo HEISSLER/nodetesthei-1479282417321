ede5af7aa15dd152b2ab5ddb5b25c4d2d50ecbda
HEISSLER <h.eissler@de.ibm.com>
2016-11-23 14:40:28 +0100

    update

---
+++ travis.sh
@@ -0,0 +1,12 @@
+body='{
+"request": {
+  "branch":"master"
+}}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token w5eY_hNqr1LKXqOCT_W2uA" \
+  -d "$body" \
+  https://api.travis-ci.org/repo/HEISSLER%2Fnodetravis/requests

a7c301bd5c0a5b0c00782e0f9fbd779f38b1943c
HEISSLER <h.eissler@de.ibm.com>
2016-12-01 14:30:04 +0100

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,8 +1,12 @@
+
 body='{
 "request": {
   "branch":"master"
 }}'
 
+
+if (env_test == 1)
+{
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
@@ -10,6 +14,4 @@ curl -s -X POST \
   -H "Authorization: token w5eY_hNqr1LKXqOCT_W2uA" \
   -d "$body" \
   https://api.travis-ci.org/repo/HEISSLER%2Fnodetravis/requests
-
-if (env_test == 0) 
-     env_test = 1;
+}

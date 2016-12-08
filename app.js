01696f9eaaaa128ad604ce1477548b314035ed4e
HEISSLER <h.eissler@de.ibm.com>
2016-12-08 07:56:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/hello', function (req, res) {
-  res.send('hello and good morning');
+  res.send('hello! good morning!');
 });
 
 app.get(express.static(__dirname + '/public'));
